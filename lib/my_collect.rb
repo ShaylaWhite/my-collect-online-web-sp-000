e9e6c2b65c80484935e370646e9ad204c2fcae37
Shayla White <shayladwhite1@gmail.com>
2019-05-15 05:36:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+  my_collect = []
+  i=0
+  while i < my_collect.length
+        yield my_collect[i].upcase.first
+        i = i + 1
+    end
+   my_collect    
 
+end
 
