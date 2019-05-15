5a602e9e91e58507dd4933ada64e52f5587f00c1
Shayla White <shayladwhite1@gmail.com>
2019-05-15 05:40:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   my_collect = []
   i=0
   while i < my_collect.length
-         my_collect << yield[i]
+         my_collect << yield[collection]
         i = i + 1
     end
    my_collect    
